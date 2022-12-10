f06cf2146db140bce27620309938c3699ad434e4
Carlos Santiago Yanzon <27785807+bizk@users.noreply.github.com>
2022-12-10 14:19:55 -0300

    Added authentication

--- src/users/users.routes.config.ts
+++ src/users/users.routes.config.ts
@@ -2,10 +2,16 @@ import {CommonRoutesConfig} from '../common/common.routes.config';
 import {Application, Request, Response, NextFunction} from 'express';
 import {USERS} from '../common/common.routes.consts';
 import {authorized, getUserInfo} from '../common/authorization.service';
+import {UsersController} from './users.controller';
+import logger from 'node-color-log';
+import {UserInterface} from './User.entity';
 
 export class UsersRoutes extends CommonRoutesConfig {
+  private usersController: UsersController;
+
   constructor(app: Application) {
     super(app, 'UsersRoutes');
+    this.usersController = new UsersController();
   }
   configureRoutes() {
     // @TODO: User CRUD routes
@@ -15,13 +21,26 @@ export class UsersRoutes extends CommonRoutesConfig {
         res.status(200).send('TODO: List of users');
       })
       .post((req: Request, res: Response) => {
+        logger.info('Registering user:', req.body);
+        this.usersController
+          .registerUser(req.body.address, req.body.secret)
+          .then((registeredUser: boolean) => {
+            if (!registeredUser) {
+              logger.warn('User could not be created', req.body);
+              res.status(500).send(false);
+            }
+            res.status(200).send(true);
+          })
+          .catch((error: any) => {
+            logger.error('Something went wrong at registering user', error);
+            res.status(500).send();
+          });
         res.status(200).send('TODO: Post new user');
       });
 
     this.app
       .route(`/${USERS}/:userId`)
       .all((req: Request, res: Response, next: NextFunction) => {
-        // Middleware executed on every route. @TODO: Validation @TODO: Authentication
         if (!authorized(req.params.userId, req.headers.authorization)) {
           console.debug(
             '[Authorization] Failed authentication for user: ',
@@ -39,7 +58,33 @@ export class UsersRoutes extends CommonRoutesConfig {
         next();
       })
       .get((req: Request, res: Response) => {
-        res.status(200).send(getUserInfo(req.params.userId));
+        logger.info('Authenticating user:', req.params.userId, req.params);
+        this.usersController
+          .loginUser(req.params.userId, '')
+          .then((userInfo: UserInterface) => {
+            if (!userInfo) {
+              logger.warn('User not found', req.params.userId, req.params);
+              res.status(500).send({
+                address: '',
+                name: '',
+                availableSpace: 0,
+                usedSpace: 0,
+              });
+            }
+            res.status(200).send({
+              address: userInfo.address,
+              name: userInfo.name,
+              availableSpace: userInfo.availableSpace,
+              usedSpace: userInfo.usedSpace,
+            });
+          })
+          .catch((error: any) => {
+            logger.error(
+              'Something went wrong at get request on user.routes',
+              error
+            );
+            res.status(500).send();
+          });
       })
       .patch((req: Request, res: Response) => {
         res
